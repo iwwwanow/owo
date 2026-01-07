d5e5b1253e0ae6d96dfc3a12ece5842b7ddf04a7
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-01-07 21:44:10 +0300

    refactor(scripts): docker entrypoint

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,30 +4,10 @@ set -e
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
+# TODO: erro if not exist
 if [ -f /root/.ssh/authorized_keys ]; then
     chown root:root /root/.ssh/authorized_keys
     chmod 600 /root/.ssh/authorized_keys
-fi
-
-if [ -n "$SSH_PUBLIC_KEY" ]; then
-    echo "$SSH_PUBLIC_KEY" > /root/.ssh/authorized_keys
-    chown root:root /root/.ssh/authorized_keys
-    chmod 600 /root/.ssh/authorized_keys
-fi
-
-# TODO: is it needed?
-if [ -f /authorized_keys.default ]; then
-    cp /authorized_keys.default /root/.ssh/authorized_keys
-    chown root:root /root/.ssh/authorized_keys
-    chmod 600 /root/.ssh/authorized_keys
-fi
-
-if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-    echo "Generating SSH host keys..."
-    ssh-keygen -A
-
-    chmod 600 /etc/ssh/ssh_host_*_key
-    chmod 644 /etc/ssh/ssh_host_*_key.pub
 fi
 
 chmod 600 /etc/ssh/ssh_host_*_key 2>/dev/null || true
