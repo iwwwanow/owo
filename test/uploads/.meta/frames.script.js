4ad7503362cdc6e9b5ea5b53d6a5527e8a8b611c
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-05-16 14:54:14 +0300

    test(uploads): open frames-grid-layout logic

--- test/uploads/.meta/frames.script.js
+++ test/uploads/.meta/frames.script.js
@@ -10,12 +10,36 @@ export const makeFramesHover = (querySelector) => {
 		element.addEventListener('mouseenter', mouseEnterHandler)
 		element.addEventListener('mouseleave', mouseLeaveHandler)
 	});
+
+	// DEBUG:
+	drawFramesWrapperLayer()
+	// DEBUG:
 }
 
 export const mouseEnterHandler = (event) => {
-	console.log(event)
+	// console.log(event)
+	drawFramesWrapperLayer()
 }
 
 export const mouseLeaveHandler = (event) => {
-	console.log(event)
+	// console.log(event)
+
+	const frameWrappers = document.querySelectorAll('.frame-wrapper')
+	console.log(frameWrappers)
+	frameWrappers.forEach(wrapper => wrapper.remove())
+	// TODO: disable scroll
+}
+
+const drawFramesWrapperLayer = () => {
+	const frameWrapper = document.createElement('div')
+	const leftWrapper = document.createElement('div')
+	const rightWrapper = document.createElement('div')
+
+	frameWrapper.classList.add('frame-wrapper', 'grid')
+	leftWrapper.classList.add('left-wrapper')
+	rightWrapper.classList.add('right-wrapper')
+
+	document.body.prepend(frameWrapper);
+	frameWrapper.append(leftWrapper);
+	frameWrapper.append(rightWrapper);
 }
