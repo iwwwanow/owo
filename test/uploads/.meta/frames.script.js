fcfcd14bb7c7c56f817ebe772697bdfd7936a5ce
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-05-16 16:53:21 +0300

    test(uploads): render fonts and images

--- test/uploads/.meta/frames.script.js
+++ test/uploads/.meta/frames.script.js
@@ -1,4 +1,4 @@
-const FRAME_SRC = 'assets/frame-1.png?static'
+const FRAME_SRC = '.meta/assets/frame-1.png?static'
 
 export const makeFramesHover = (querySelector) => {
 	const gridNode = document.querySelector(querySelector)
@@ -31,15 +31,36 @@ export const mouseLeaveHandler = (event) => {
 }
 
 const drawFramesWrapperLayer = () => {
+	// TODO: get it from parent and cycle it
+	const sourceImageLink = '/_frames-script/6104c2143578475.627cf80887bb9.png?static'
+
 	const frameWrapper = document.createElement('div')
 	const leftWrapper = document.createElement('div')
 	const rightWrapper = document.createElement('div')
+	const header1 = document.createElement('h1')
+	const header2 = document.createElement('h2')
+	const header3 = document.createElement('h3')
+	const frameImage = document.createElement('img')
+	const sourceImage = document.createElement('img')
 
 	frameWrapper.classList.add('frame-wrapper', 'grid')
 	leftWrapper.classList.add('left-wrapper')
 	rightWrapper.classList.add('right-wrapper')
+	frameImage.classList.add('frame-image')
+	sourceImage.classList.add('source-image')
+
+	header1.innerText = 'Шаман'
+	header2.innerText = 'шаман 1.25x1.25 - акрил, осп-плита, лак'
+	header3.innerText = 'price'
+	frameImage.src = FRAME_SRC;
+	sourceImage.src = sourceImageLink
 
 	document.body.prepend(frameWrapper);
 	frameWrapper.append(leftWrapper);
 	frameWrapper.append(rightWrapper);
+	leftWrapper.appendChild(header1)
+	leftWrapper.appendChild(header2)
+	leftWrapper.appendChild(header3)
+	rightWrapper.appendChild(frameImage)
+	rightWrapper.appendChild(sourceImage)
 }
