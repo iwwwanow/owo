2fb1b005bee4e2e5536531622a756c52e4c9f733
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-06-17 13:39:38 +0300

    feat(meta): additional content init

--- test/uploads/.meta/index.js
+++ test/uploads/.meta/index.js
@@ -1,5 +1,7 @@
 import "./common.script.js?static"
 
-import {makeFramesHover} from './frames.script.js?static'
+import { makeFramesHover } from './frames.script.js?static'
+import { renderAdditionalContent } from "./additional-content.script.js?static"
 
 makeFramesHover('#grid-frames-script')
+renderAdditionalContent('#grid-frames-script-2')
